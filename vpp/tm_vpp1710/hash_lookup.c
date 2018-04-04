e921a45fa40de1a5bf087cbd1514d2e03db3c58c
valebru <valebrshk@gmail.com>
2018-04-04 13:30:23 +0200

    TupleMerge final version

--- vpp/tm_vpp1710/hash_lookup.c
+++ vpp/tm_vpp1710/hash_lookup.c
@@ -1003,9 +1003,9 @@ show_hash_acl_hash (vlib_main_t * vm, acl_main_t *am, u32 verbose)
 /*==================================================================*/
 /*
 Code adapted by Valerio Bruschi (valerio.bruschi@telecom-paristech.fr) 
-and based on the TupleMerge [1] simulator kindly made available 
-(at URL, if any) by  James Daly (dalyjamese@gmail.com) 
-and  Eric Torng (torng@cse.msu.edu)
+and based on the TupleMerge [1] simulator kindly made available
+by  James Daly (dalyjamese@gmail.com) and  Eric Torng (torng@cse.msu.edu)
+( http://www.cse.msu.edu/~dalyjame/ or http://www.cse.msu.edu/~torng/ )
 
 [1] James Daly, Eric Torng "TupleMerge: Building Online Packet Classifiers 
 by Omitting Bits", In Proc. IEEE ICCCN 2017, pp. 1-10
